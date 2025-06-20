56e96cae9f4c6a26ad584fd6f595c87dcde7521d
inkstray <80412260+inkstray@users.noreply.github.com>
2025-06-20 11:50:49 +0200

    Update qr.go

--- qart/qr.go
+++ qart/qr.go
@@ -17,10 +17,10 @@ import (
 	"sort"
 	"time"
 
-	"rsc.io/qr"
-	"rsc.io/qr/coding"
-	"rsc.io/qr/gf256"
-	"rsc.io/qr/qart/internal/resize"
+	"github.com/inkstray/rsc-qr"
+	"github.com/inkstray/rsc-qr/coding"
+	"github.com/inkstray/rsc-qr/gf256"
+	"github.com/inkstray/rsc-qr/qart/internal/resize"
 )
 
 type Image struct {
